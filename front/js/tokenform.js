c15ce006dbc405ad515b7a4e8a5df552b72ba405
Yan BUATOIS <dracoctix@outlook.fr>
2018-10-28 19:04:30 +0100

    User can now log as the bot, and he has a new window.

--- front/js/tokenform.js
+++ front/js/tokenform.js
@@ -1,1 +1,17 @@
-$("#tokenform").on("submit", () => false);
+const ipc = require('electron').ipcRenderer;
+
+function disableForm(disabled) {
+    $('#sendButton').prop("disabled", disabled);
+    $('#token').prop("disabled", disabled);
+}
+
+$("#tokenform").on("submit", () => {
+    let token = $('#token');
+    disableForm(true);
+    ipc.send("tokenSent", token.val());
+    return false;
+});
+
+ipc.on('tokenFailed', () => {
+    disableForm(false);
+});
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const electron = require('electron');
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, ipcMain} = electron;
+const Discord = require('discord.js');
+const discordClient = new Discord.Client();
 
-let tokenWindow;
 
+let tokenWindow, mainWindow;
+
+/**
+ * Start of windows management
+ */
 function createTokenWindow() {
     tokenWindow = new BrowserWindow({
         width: 400,
@@ -20,7 +26,67 @@ function createTokenWindow() {
     });
 }
 
+function createMainWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 750,
+        title: "Bot management",
+        resizable: true,
+        minimizable: true,
+        maximizable: true,
+        autoHideMenuBar: true,
+    });
+
+    mainWindow.loadFile('front/main.html');
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
 app.on('window-all-closed', () => {
     app.quit();
 });
 app.on('ready', createTokenWindow);
+
+/**
+ * End of Windows Management
+ * Start of Discord Bot Management
+ */
+discordClient.on('ready', () => {
+    createMainWindow();
+    tokenWindow.close();
+});
+
+discordClient.on('error', (err) => {
+    if(tokenWindow !== null) {
+        electron.dialog.showMessageBox(tokenWindow, {
+            type: 'error',
+            title: 'Bot connexion failed',
+            message: `The connexion to the bot failed.<br />(Error : <strong>${err.message}</strong>)`,
+        },() => {
+            tokenWindow.webContents().send('tokenFailed');
+        });
+    }
+});
+/**
+ * End of Discord Bot Management
+ * Start of IPC Management
+ */
+
+// We manage now IPC :
+ipcMain.on('tokenSent', (event, token) => {
+    const loginPromise = discordClient.login(token);
+    loginPromise.then(null, (err) => {
+        electron.dialog.showMessageBox(tokenWindow, {
+            type: 'error',
+            title: 'Bot connexion failed',
+            message: `The connexion to the bot failed.\n(Error : ${err.message})`,
+        },() => {
+            tokenWindow.webContents.send('tokenFailed');
+        });
+    });
+});
+
+/**
+ * End of IPC Management
+ */
