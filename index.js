72e4b25b3319f407e0f158231b7ed222c54d622b
sentrycrash <34958963+sencrash@users.noreply.github.com>
2019-11-25 00:36:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ ipcMain.on('changesrv', (event, id) => {
 ipcMain.on('changechannel', (event, id) => {
     actualChannel = discordClient.channels.get(id);
     actualChannel.fetchMessages({ limit: 100 })
-        .then(messages => messages.reverse().forEach(messagestowindow))
+        .then(messages => messages.first(100).reverse().forEach(messagestowindow))
         .catch(console.error);
     event.sender.send('channelok');
 });
